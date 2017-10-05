10ef3964dd026c75b501a029a974eece4397b8c8
fishergio <spascualgmx@gmail.com>
2017-10-04 20:05:20 -0500

    Configuracion del proyecto y maquetado del header y seccion de features

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ const gulp = require('gulp'),
     connect = require('gulp-connect'),
     pug = require('gulp-pug'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
-dir = {
-    src: 'src',
-    dist: 'dist',
-    nm: 'node_modules'
-},
+    useref = require('gulp-useref'),
+    concat = require('gulp-concat'),
+    uncss = require('gulp-uncss'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    htmlmin = require('gulp-htmlmin'),
+    browserSync = require('browser-sync').create(),
+    
+    dir = {
+        src: 'app',
+        dist: 'dist',
+        nm: 'node_modules'
+    },
     files = {
         CSS: [
-            `${dir.nm}/animate.css/animate.min.css`,
+            /*`${dir.nm}/animate.css/animate.min.css`,
             `${dir.nm}/font-awesome/css/font-awesome.min.css`,
-            `${dir.nm}/responsimple/responsimple.min.css`,
-            `/css/estilos.css`
+            `${dir.nm}/responsimple/responsimple.min.css`,*/
+            `${dir.src}/lib/css/estilos.css`
         ],
         mCSS: 'estilos.min.css',
         JS: [
@@ -33,32 +40,45 @@ dir = {
             }
         },
         sass: { outputStyle: 'compressed' },
+        uncss: { html: [`${dir.dist}/*.html`] },
+        autoprefixer: {
+            browsers: ['last 5 versions'],
+            cascade: false
+        },
+        htmlmin: { collapseWhitespace: true }
     };
 
-gulp.task('server', ['sass', 'pug'], () => {
+    var dir_app = {
+        sass: './app/scss/*.scss',
+        js: './app/js/**/*.js',
+        html: './app/html/**/*.html',
+        lib: './app/lib/**/*.css'
+      }   
+
+
+gulp.task('default', ['sass', 'pug'], () => {
 
     browserSync.init({
-        server: "./dist"
+        server: "./app"
     });
 
-    gulp.watch("./src/scss/*.scss", ['sass']);
-    gulp.watch("./src/pug/*.pug").on('change', browserSync.reload);
-    gulp.watch("./src/scss/*.scss").on('change', browserSync.reload);
+    gulp.watch("./app/*.html", ['pug']);
+    gulp.watch("./app/scss/*.scss", ['sass']);
 });
 
 gulp.task('pug', () => {
     gulp
-        .src(`${dir.src}/pug/*.pug`)
-        .pipe(pug(opts.pug))
+        .src(`${dir.src}/*.html`)
+        //.pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest(dir.dist))
         .pipe(browserSync.stream());
 });
 
 gulp.task('sass', () => {
     gulp
-        .src(`${dir.src}/scss/*.scss`)
+        .src(`${dir_app.sass}`)
         .pipe(sass(opts.sass))
-        .pipe(gulp.dest(`${dir.dist}/css`))
+        .pipe(gulp.dest('./app/lib/css'))
         .pipe(browserSync.stream());
 });
 
@@ -70,10 +90,29 @@ gulp.task('es6', () => {
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./src/pug/*.pug', ['pug']);
-    gulp.watch('./src/scss/*.scss', ['sass']);
+    gulp.watch('./app/*.index', ['pug']);
+    gulp.watch('./app/scss/*.scss', ['sass', 'css']);
 })
 
-gulp.task('default', ['server', 'watch']);
+//gulp.task('default', ['server', 'watch']);
 
 
+
+
+gulp.task('css', () => {
+    gulp
+        .src(['dir_app.lib', 'dir_app.scss'])
+        .pipe(concat(files.mCSS))
+        .pipe(uncss(opts.uncss))
+        .pipe(autoprefixer(opts.autoprefixer))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(`${dir.dist}/css`));
+});
+
+gulp.task('html', () => {
+    gulp
+        .src(`${dir.dist}/*.html`)
+        .pipe(useref())
+        .pipe(htmlmin(opts.htmlmin))
+        .pipe(gulp.dest(dir.dist));
+});
